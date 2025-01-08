fa89fb9752d5c008aa3d099eecbc706276ecab91
Muhammed <olayinkafacus@gmail.com>
2025-01-07 22:45:31 -0500

    Initial Commit

---
+++ FastingApp/FastingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingAppApp.swift
+//  FastingApp
+//
+//  Created by Yinka  Facus  on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FastingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
